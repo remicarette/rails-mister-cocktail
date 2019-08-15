72157834402de3a1675b4960aa725683fa95f141
remi carette <carette.remi@gmail.com>
2019-08-15 16:15:05 +0200

    adding root to cocktails#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # list all cocktails
+
+  root to: 'cocktails#index'
+
   resources :cocktails, only: [:index, :show, :new, :create] do
       resources :doses, only: [:new, :create]
     end
