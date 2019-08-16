cf7d4a9ab567a94e32327064d79e573aa8a7acc8
remi carette <carette.remi@gmail.com>
2019-08-16 17:04:57 +0200

    remove route /doses/new

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@
 
 
   resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
-      resources :doses, only: [:new, :create]
+      resources :doses, only: [:create]
     end
 
   resources :doses, only: :destroy
